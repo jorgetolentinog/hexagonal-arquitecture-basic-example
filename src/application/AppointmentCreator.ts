d6ed4daa978d9397b5285ad8c2ba1c7f63091325
Jorge Tolentino <jorge.tolentino.g@gmail.com>
2023-05-05 07:20:51 -0500

    wip

--- src/application/AppointmentCreator.ts
+++ src/application/AppointmentCreator.ts
@@ -1,8 +1,36 @@
+import { AppointmentRepository } from "../domain/repositories/AppointmentRepository";
+import { UniqueId } from "../domain/ports/UniqueId";
+import { Mailer } from "../domain/ports/Mailer";
+import { Appointment } from "../domain/entities/Appointment";
+
 export class AppointmentCreator {
-  async execute() {
-    /**
-     * 1. Crear reserva
-     * 2. Enviar correo de confirmación
-     */
+  private _bookingRepository: AppointmentRepository;
+  private _uniqueId: UniqueId;
+  private _mailer: Mailer;
+
+  constructor(props: {
+    bookingRepository: AppointmentRepository;
+    uniqueId: UniqueId;
+    mailer: Mailer;
+  }) {
+    this._bookingRepository = props.bookingRepository;
+    this._uniqueId = props.uniqueId;
+    this._mailer = props.mailer;
+  }
+
+  async execute(props: { date: string; email: string }) {
+    const appointment = Appointment.create({
+      id: this._uniqueId.generate(),
+      date: props.date,
+      email: props.email,
+    });
+
+    await this._bookingRepository.save(appointment);
+
+    await this._mailer.send({
+      to: appointment.email,
+      subject: "Reserva creada",
+      body: `Tu reserva de el ${appointment.date} ha sido creada`,
+    });
   }
 }
