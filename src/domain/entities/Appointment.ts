f0c370737c7e60682143ae89e311eeac2f415691
Jorge Tolentino <jorge.tolentino.g@gmail.com>
2023-05-05 07:14:54 -0500

    wip

---
+++ src/domain/entities/Appointment.ts
@@ -0,0 +1,29 @@
+export class Appointment {
+  id: string;
+  date: string;
+  email: string;
+  createdAt: string;
+
+  constructor(props: { id: string; date: string; email: string }) {
+    this.id = props.id;
+    this.date = props.date;
+    this.email = props.email;
+  }
+
+  static create(props: {
+    id: string;
+    date: string;
+    email: string;
+  }): Appointment {
+    if (props.date >= new Date().toISOString()) {
+      throw new Error("Date cannot be in the future");
+    }
+
+    return {
+      id: props.id,
+      date: props.date,
+      email: props.email,
+      createdAt: new Date().toISOString(),
+    };
+  }
+}
